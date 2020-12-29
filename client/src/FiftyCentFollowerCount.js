83a29dbc33d0eb04ce9e459e4b87e12ad67672e7
Zac Colley <zac@clubpenguin.email>
2020-12-29 17:56:44 +0000

    improve fiddy

--- client/src/FiftyCentFollowerCount.js
+++ client/src/FiftyCentFollowerCount.js
@@ -4,16 +4,18 @@ import "./FiftyCentFollowerCount.css";
 
 const FiftyCentFollowerCount = ({ followTotal }) => (
   <div className="FiftyCentFollowerCount">
-    <img
-      className="FiftyCentFollowerCount__background"
-      src="50cent.png"
-      alt=""
-    />
-    <div
-      className="FiftyCentFollowerCount__bar"
-      style={{ height: `${(followTotal / 50) * 100}%` }}
-    >
-      <img src="50cent.png" alt="" />
+    <div class="FiftyCentFollowerCount__progress">
+      <img
+        className="FiftyCentFollowerCount__progress__background"
+        src="50cent.png"
+        alt=""
+      />
+      <div
+        className="FiftyCentFollowerCount__progress__bar"
+        style={{ height: `${(followTotal / 50) * 100}%` }}
+      >
+        <img src="50cent.png" alt="" />
+      </div>
     </div>
     <span className="FiftyCentFollowerCount__target">
       {followTotal}cent / 50cent followers

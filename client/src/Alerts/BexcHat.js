42adf712e9bd1312135b4322c1fc671c5b0ad4a9
Zac Colley <zac@clubpenguin.email>
2021-02-03 22:12:52 +0000

    update bex alert

--- client/src/Alerts/BexcHat.js
+++ client/src/Alerts/BexcHat.js
@@ -20,8 +20,20 @@ const BexcHat = ({ currentFaceDetection }) => {
   };
 
   return (
-    <div className="BexcHat" style={positionStyles}>
-      <div className="BexcHat__hat" />
+    <div className="BexcHat">
+      <div className="BexcHat__hat--wrapper" style={positionStyles}>
+        <div className="BexcHat__hat" />
+      </div>
+      <img
+        className="BexcHat__heaven"
+        src="/alerts/bexchat-heaven.png"
+        alt=""
+      />
+      <img
+        className="BexcHat__james-corden"
+        src="/alerts/bexchat-james-corden.png"
+        alt=""
+      />
     </div>
   );
 };

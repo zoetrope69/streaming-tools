de6fd8d931c69e00f6b987ef7a00ed57df16af01
Zac Colley <zac@clubpenguin.email>
2021-08-07 18:06:39 +0100

    go back to opencv instead of google

--- src/helpers/detect-faces.js
+++ src/helpers/detect-faces.js
@@ -1,64 +1,35 @@
-const fs = require("fs");
-const vision = require("@google-cloud/vision");
-
+const cv = require("opencv4nodejs");
 const bufferFromBase64 = require("./buffer-from-base64");
 
-const client = new vision.ImageAnnotatorClient();
-
-async function saveBase64Image(base64) {
-  const { buffer, contentType } = bufferFromBase64(base64);
-  const imagePath = `temp-face-image.${contentType}`;
-  fs.writeFileSync(imagePath, buffer);
-  return imagePath;
-}
-
-async function detectFaces(base64) {
-  const imagePath = await saveBase64Image(base64);
-
-  const [faceDetectionResult] = await client.faceDetection(imagePath);
-
-  if (
-    !faceDetectionResult ||
-    faceDetectionResult.faceAnnotations.length === 0
-  ) {
-    throw new Error("No faces detected!");
-  }
-
-  const [face] = faceDetectionResult.faceAnnotations.sort(
-    (a, b) => b.detectionConfidence - a.detectionConfidence
+async function detectFaces(dataUri) {
+  const { buffer: imageBuffer } = bufferFromBase64(dataUri);
+  const cvImage = cv.imdecode(imageBuffer);
+  const classifier = new cv.CascadeClassifier(
+    cv.HAAR_FRONTALFACE_ALT2
   );
 
-  if (face.detectionConfidence < 0.2) {
+  // detect faces
+  const { objects, numDetections: confidences } =
+    classifier.detectMultiScale(cvImage.bgrToGray());
+
+  if (!objects.length) {
     throw new Error("No faces detected!");
   }
 
-  // boundingPoly - full head
-  // fdBoundingPoly - just face
-  const { vertices } = face.fdBoundingPoly;
-  /*
-    [
-      { x: 64,  y: 77  },  top left
-      { x: 240, y: 77  },  top right
-      { x: 240, y: 262 },  bottom right
-      { x: 64,  y: 262 }   bottom left
-    ]
-  */
+  const results = objects
+    .map((object, i) => {
+      const confidence = confidences[i];
+      return { rect: object, confidence };
+    })
+    .sort((a, b) => b.confidence - a.confidence);
 
-  const xYPositions = vertices[0];
-  const bottomRightXyPositions = vertices[2];
+  const [bestResult] = results;
 
-  const { x, y } = xYPositions;
-  const width = bottomRightXyPositions.x - x;
-  const height = bottomRightXyPositions.y - y;
+  const { rect, confidence } = bestResult;
 
   return {
-    position: {
-      x,
-      y,
-      width,
-      height,
-    },
-    confidence: face.detectionConfidence,
+    position: rect,
+    confidence,
   };
 }
 

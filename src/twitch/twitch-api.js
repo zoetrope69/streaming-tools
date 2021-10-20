0f2df9d53e9b73bc56d0c30d55d732a29b5f83a9
Zac Colley <zac@clubpenguin.email>
2021-10-20 18:16:27 +0100

    move twitch api functions out of main function

--- src/twitch/twitch-api.js
+++ src/twitch/twitch-api.js
@@ -72,246 +72,244 @@ async function callTwitchAPI({
   return json;
 }
 
+async function getUser(username) {
+  const response = await callTwitchAPI({
+    endpoint: "users",
+    options: {
+      login: username,
+    },
+  });
+
+  if (!response || !response.data || response.data.length === 0) {
+    return null;
+  }
+
+  const [userData] = response.data;
+  const { display_name, profile_image_url } = userData;
+
+  return {
+    username: display_name,
+    image: profile_image_url,
+  };
+}
+
+async function getChannelInfo() {
+  const response = await callTwitchAPI({
+    endpoint: "channels",
+    options: {
+      broadcaster_id: TWITCH_BROADCASTER_ID,
+    },
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return {};
+  }
+
+  const {
+    broadcaster_id,
+    broadcaster_name,
+    broadcaster_language,
+    title,
+    game_id,
+    game_name,
+  } = data[0];
+
+  return {
+    id: broadcaster_id,
+    username: broadcaster_name,
+    title,
+    language: broadcaster_language,
+    categoryId: game_id,
+    categoryName: game_name,
+  };
+}
+
+async function setChannelInfo({ category, title }) {
+  const newChannelInfo = {};
+
+  if (category) {
+    newChannelInfo.game_id = category.id;
+  }
+
+  if (title) {
+    newChannelInfo.title = title;
+  }
+
+  return callTwitchAPI({
+    endpoint: "channels",
+    options: {
+      broadcaster_id: TWITCH_BROADCASTER_ID,
+    },
+    fetchOptions: {
+      method: "PATCH",
+      body: JSON.stringify(newChannelInfo),
+    },
+  });
+}
+
+async function getEventSubSubscriptions() {
+  const response = await callTwitchAPI({
+    endpoint: "eventsub/subscriptions",
+    type: "twitch-app",
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return [];
+  }
+
+  return data;
+}
+
+async function createEventSubSubscription({ type, ngrokUrl }) {
+  const response = await callTwitchAPI({
+    endpoint: "eventsub/subscriptions",
+    fetchOptions: {
+      method: "POST",
+      body: JSON.stringify({
+        type,
+        version: "1",
+        condition: {
+          broadcaster_user_id: TWITCH_BROADCASTER_ID,
+        },
+        transport: {
+          method: "webhook",
+          callback: `${ngrokUrl}/eventSubCallback`,
+          secret: TWITCH_EVENTSUB_SECRET,
+        },
+      }),
+    },
+    type: "twitch-app",
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return {};
+  }
+
+  return data;
+}
+
+async function deleteEventSubSubscription({ id }) {
+  const response = await callTwitchAPI({
+    endpoint: "eventsub/subscriptions",
+    options: { id },
+    fetchOptions: {
+      method: "DELETE",
+    },
+    type: "twitch-app",
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return {};
+  }
+
+  return data;
+}
+
+async function getGlobalEmotes() {
+  const response = await callTwitchAPI({
+    endpoint: "chat/emotes/global",
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return {};
+  }
+
+  return data;
+}
+
+async function getChannelEmotes() {
+  const response = await callTwitchAPI({
+    endpoint: "chat/emotes",
+    options: {
+      broadcaster_id: TWITCH_BROADCASTER_ID,
+    },
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return {};
+  }
+
+  return data;
+}
+
+async function getEmotes() {
+  const channelEmotes = await getChannelEmotes();
+  const globalEmotes = await getGlobalEmotes();
+
+  return [...channelEmotes, ...globalEmotes].map((emote) => {
+    return {
+      id: emote.id,
+      code: emote.name,
+      image: emote?.images?.url_4x,
+    };
+  });
+}
+
+async function getTags() {
+  const response = await callTwitchAPI({
+    endpoint: "streams/tags",
+    options: {
+      broadcaster_id: TWITCH_BROADCASTER_ID,
+    },
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return null;
+  }
+
+  return data;
+}
+
+async function setTags(tagIds = []) {
+  const response = await callTwitchAPI({
+    endpoint: "streams/tags",
+    options: {
+      broadcaster_id: TWITCH_BROADCASTER_ID,
+    },
+    fetchOptions: {
+      method: "PUT",
+      body: JSON.stringify({ tag_ids: tagIds }),
+    },
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return null;
+  }
+
+  return data;
+}
+
+async function searchCategories(query) {
+  const response = await callTwitchAPI({
+    endpoint: `search/categories?query=${encodeURIComponent(query)}`,
+  });
+
+  const { data } = response;
+
+  if (!data || data.length === 0) {
+    return [];
+  }
+
+  return data;
+}
+
 async function TwitchAPI({ ngrokUrl }) {
-  async function getUser(username) {
-    const response = await callTwitchAPI({
-      endpoint: "users",
-      options: {
-        login: username,
-      },
-    });
-
-    if (!response || !response.data || response.data.length === 0) {
-      return null;
-    }
-
-    const [userData] = response.data;
-    const { display_name, profile_image_url } = userData;
-
-    return {
-      username: display_name,
-      image: profile_image_url,
-    };
-  }
-
-  async function getChannelInfo() {
-    const response = await callTwitchAPI({
-      endpoint: "channels",
-      options: {
-        broadcaster_id: TWITCH_BROADCASTER_ID,
-      },
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return {};
-    }
-
-    const {
-      broadcaster_id,
-      broadcaster_name,
-      broadcaster_language,
-      title,
-      game_id,
-      game_name,
-    } = data[0];
-
-    return {
-      id: broadcaster_id,
-      username: broadcaster_name,
-      title,
-      language: broadcaster_language,
-      categoryId: game_id,
-      categoryName: game_name,
-    };
-  }
-
-  async function setChannelInfo({ category, title }) {
-    const newChannelInfo = {};
-
-    if (category) {
-      newChannelInfo.game_id = category.id;
-    }
-
-    if (title) {
-      newChannelInfo.title = title;
-    }
-
-    return callTwitchAPI({
-      endpoint: "channels",
-      options: {
-        broadcaster_id: TWITCH_BROADCASTER_ID,
-      },
-      fetchOptions: {
-        method: "PATCH",
-        body: JSON.stringify(newChannelInfo),
-      },
-    });
-  }
-
-  async function getEventSubSubscriptions() {
-    const response = await callTwitchAPI({
-      endpoint: "eventsub/subscriptions",
-      type: "twitch-app",
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return [];
-    }
-
-    return data;
-  }
-
-  async function createEventSubSubscription({ type, ngrokUrl }) {
-    const response = await callTwitchAPI({
-      endpoint: "eventsub/subscriptions",
-      fetchOptions: {
-        method: "POST",
-        body: JSON.stringify({
-          type,
-          version: "1",
-          condition: {
-            broadcaster_user_id: TWITCH_BROADCASTER_ID,
-          },
-          transport: {
-            method: "webhook",
-            callback: `${ngrokUrl}/eventSubCallback`,
-            secret: TWITCH_EVENTSUB_SECRET,
-          },
-        }),
-      },
-      type: "twitch-app",
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return {};
-    }
-
-    return data;
-  }
-
-  async function deleteEventSubSubscription({ id }) {
-    const response = await callTwitchAPI({
-      endpoint: "eventsub/subscriptions",
-      options: { id },
-      fetchOptions: {
-        method: "DELETE",
-      },
-      type: "twitch-app",
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return {};
-    }
-
-    return data;
-  }
-
-  async function getGlobalEmotes() {
-    const response = await callTwitchAPI({
-      endpoint: "chat/emotes/global",
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return {};
-    }
-
-    return data;
-  }
-
-  async function getChannelEmotes() {
-    const response = await callTwitchAPI({
-      endpoint: "chat/emotes",
-      options: {
-        broadcaster_id: TWITCH_BROADCASTER_ID,
-      },
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return {};
-    }
-
-    return data;
-  }
-
-  async function getEmotes() {
-    const channelEmotes = await getChannelEmotes();
-    const globalEmotes = await getGlobalEmotes();
-
-    return [...channelEmotes, ...globalEmotes].map((emote) => {
-      return {
-        id: emote.id,
-        code: emote.name,
-        image: emote?.images?.url_4x,
-      };
-    });
-  }
-
-  async function getTags() {
-    const response = await callTwitchAPI({
-      endpoint: "streams/tags",
-      options: {
-        broadcaster_id: TWITCH_BROADCASTER_ID,
-      },
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return null;
-    }
-
-    return data;
-  }
-
-  async function setTags(tagIds = []) {
-    const response = await callTwitchAPI({
-      endpoint: "streams/tags",
-      options: {
-        broadcaster_id: TWITCH_BROADCASTER_ID,
-      },
-      fetchOptions: {
-        method: "PUT",
-        body: JSON.stringify({ tag_ids: tagIds }),
-      },
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return null;
-    }
-
-    return data;
-  }
-
-  async function searchCategories(query) {
-    const response = await callTwitchAPI({
-      endpoint: `search/categories?query=${encodeURIComponent(
-        query
-      )}`,
-    });
-
-    const { data } = response;
-
-    if (!data || data.length === 0) {
-      return [];
-    }
-
-    return data;
-  }
-
   return {
     getUser: async (username) => {
       const user = await getUser(username);

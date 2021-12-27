ddb39e5357dddd29f3509d70fbb8821ed10ebbc1
Zac Colley <zac@clubpenguin.email>
2021-12-27 03:38:05 +0000

    tweak template

--- src/main/redemptions/_template.js
+++ src/main/redemptions/_template.js
@@ -1,3 +1,7 @@
+/* eslint-disable */
+import { setTimeout } from "timers/promises"; // eslint-disable-line node/no-missing-import
+import obs from "../../obs/index.js";
+
 import BaseRedemption from "./base-redemption.js";
 
 import Logger from "../../helpers/logger.js";
@@ -5,7 +9,7 @@ const logger = new Logger("🤓 Redemption: Template");
 
 class TemplateRedemption extends BaseRedemption {
   constructor({ io, streamingService }) {
-    const title = "dance with zac";
+    const title = "";
 
     super({ streamingService, title });
 
@@ -13,9 +17,9 @@ class TemplateRedemption extends BaseRedemption {
     this.streamingService = streamingService;
     this.data = {};
 
-    this.fufilledRedemption((data) => this.start(data));
     this.unfufilledRedemption((data) => this.start(data));
-    this.cancelledRedemption((data) => this.start(data));
+    this.fufilledRedemption((data) => this.stop(data));
+    this.cancelledRedemption((data) => this.stop(data));
   }
 
   async start(data) {

ae80b3db39a731e8aec1194d6e87943409a27ab1
Zac Colley <zac@clubpenguin.email>
2021-12-31 01:42:59 +0000

    add logging to joycons

--- src/joycons.js
+++ src/joycons.js
@@ -1,11 +1,16 @@
 import { EventEmitter } from "events";
 
+import Logger from "./helpers/logger.js";
+const logger = new Logger("🎮 Joycons");
+
 class Joycons extends EventEmitter {
   constructor({ app }) {
     super();
 
     app.post("/windows/joycons", (request, response) => {
       const { data } = request.body;
+
+      logger.debug({ data });
 
       response.json({ success: true });
 

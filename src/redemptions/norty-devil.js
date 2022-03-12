e1d7acddbb5266e2b48e9896707c4647eb611e77
Zac Colley <zac@clubpenguin.email>
2022-03-12 18:42:53 +0000

    add lights when arse redemption happens

--- src/redemptions/norty-devil.js
+++ src/redemptions/norty-devil.js
@@ -1,5 +1,6 @@
 import { setTimeout } from "timers/promises"; // eslint-disable-line node/no-missing-import
 import obs from "../obs/index.js";
+import { setLightsColor, resetLights } from "../helpers/hue-bulbs.js";
 
 import BaseRedemption from "./base-redemption.js";
 
@@ -29,11 +30,33 @@ class NortyDevilRedemption extends BaseRedemption {
   async start() {
     logger.log("Triggered...");
     const timeout = 20 * 1000;
+    this.alarmColours(timeout);
     obs.turnOnOverlay("Stop Look At My Giant Ass", timeout);
     await setTimeout(timeout);
     this.streamingService.chat.sendMessage(
       `shout-out to twitch.tv/EggEllie the creator of the norty devils and twitch.tv/Broomyjag for the voice of the devil`
     );
+  }
+
+  async alarmColours(timeout) {
+    const cycleDuration = 2000;
+
+    const cycleLights = async () => {
+      setLightsColor("black");
+      await setTimeout(cycleDuration);
+      setLightsColor("red");
+    };
+    const lightsCycleInterval = setInterval(
+      cycleLights,
+      cycleDuration * 2
+    );
+    cycleLights();
+
+    // stop the colours after certain time
+    await setTimeout(timeout * 0.66);
+    clearInterval(lightsCycleInterval);
+    await setTimeout(cycleDuration * 2);
+    await resetLights();
   }
 }
 
